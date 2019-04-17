5538b0f7d911da3f8a34c44ef7d07093d709e032
Kev Cunningham <kevin@cunningtech.co.uk>
2019-04-17 21:36:37 +0100

    Removed AWS provider

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    region     = "${var.region}"
-}
+#provider "aws" {
+#    access_key = "${var.access_key}"
+#    secret_key = "${var.secret_key}"
+#    region     = "${var.region}"
+#}
 
 resource "aws_instance" "DC1" {
   ami           = "${var.DC_ami}"
