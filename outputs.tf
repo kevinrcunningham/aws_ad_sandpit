6a466a007f588c28be6cf04c349dc8663ec59a31
Kev Cunningham <kevin@cunningtech.co.uk>
2019-04-17 21:35:13 +0100

    Added additional useful output variables for module

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "DC__Public_IP" {
 output "DC_Private_IP" {
     value = "${aws_instance.DC1.private_ip}"
 }
+
+output "sandpit_subnet1_id" {
+    value = "${aws_subnet.sandpit_subnet1.id}"
+}
+
+output "sandpit_subnet2_id" {
+    value = "${aws_subnet.sandpit_subnet2.id}"
+}
+
+output "vpc_cidr_block" {
+    value = "${var.vpc_cidr_block}"
+}
+
+output "vpc_id" {
+    value = "${aws_vpc.sandpitVPC.id}"
+}
